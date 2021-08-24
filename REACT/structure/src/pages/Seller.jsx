c4dff6ad24d7a331edbea066d0bf6c7b711d4e09
Andrey Yakovenko <yakovenkoandyua@gmail.com>
2021-08-24 22:54:03 +0300

    fix

--- REACT/structure/src/pages/Seller.jsx
+++ REACT/structure/src/pages/Seller.jsx
@@ -1,42 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import s from './Seller.module.css'
 import Tabs from '../components/Tabs/Tabs'
+import { shops } from '../shops'
+const Seller = ({ id }) => {
+	const [sellerData, setSellerData] = useState({})
 
-const Seller = ({ filteredShop }) => {
-    const [sellerData, setSellerData] = useState(null)
-    
-    useEffect(() => {
-        setSellerData(filteredShop)
-    }, [filteredShop])
-    console.log(sellerData);
-    // const {link, delivery, id} = sellerData;
+	const getShopById = id => {
+		const shopId = shops.filter(shop => shop.id === id)
+		setSellerData(shopId[0])
+	}
 
-    // console.log(sellerData);
+	useEffect(() => {
+		getShopById(id)
+	}, [id])
 
-    return (
-        <div className={s.wrapper}>
-            <div className={s.inner}>
-                <div className={s.left}>
-                    <h1 className={s.title}>{sellerData.id}</h1>
-                    <div className={s.link}>
-                        <div className={s.subtitle}>Site: </div>
-                        <a href={sellerData.link}>{sellerData.link}</a>
-                    </div>
-                    <div className={s.info}>
-                        <div className={s.subtitle}>Delivery terms: </div>
-                        <div className={s.delivery}>
-                            {sellerData.delivery}
-                        </div>
-                    </div>
-                </div>
-                <div className={s.right}>
-                    <div className={s.logo}>LOGO</div>
-                </div>
-            </div>
-            <Tabs />
-        </div>
-    )
+	return (
+		<div className={s.wrapper}>
+			<div className={s.inner}>
+				<div className={s.left}>
+					<h1 className={s.title}>{sellerData.id}</h1>
+					<div className={s.link}>
+						<div className={s.subtitle}>Site: </div>
+						<a href={sellerData.link}>{sellerData.link}</a>
+					</div>
+					<div className={s.info}>
+						<div className={s.subtitle}>Delivery terms: </div>
+						<div className={s.delivery}>{sellerData.delivery}</div>
+					</div>
+				</div>
+				<div className={s.right}>
+					<div className={s.logo}>LOGO</div>
+				</div>
+			</div>
+			<Tabs />
+		</div>
+	)
 }
 
 export default Seller
-
